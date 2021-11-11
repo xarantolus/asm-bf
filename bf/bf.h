51e96715a7eb6c41bb4891f4673bc135a521bb8b
xarantolus <xarantolus@protonmail.com>
2021-11-11 16:42:25 +0100

    Initial instructions

--- bf/bf.h
+++ bf/bf.h
@@ -1,6 +1,9 @@
 #include <stdint.h>
 
+#define BF_TAPE_OVERFLOW -3
+#define BF_TAPE_UNDERFLOW -4
+
 // bf_execute executes the given brainfuck program (zero-terminated string)
-// on the given band (starting at position 0) of length band_length
+// on the given tape (starting at position 0) of length tape_length
 // it returns an exit code of 0 on success
-extern int bf_execute(const char *program, uint8_t *band, uint32_t band_length);
+extern int bf_execute(const char *program, uint8_t *tape, uint32_t tape_length);
