61bc3124ed24a9b92f1a91fed040245c9baa4941
xarantolus <xarantolus@protonmail.com>
2021-11-11 18:01:58 +0100

    Print/Read stubs

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int main(int argc, char const *argv[])
 {
     const size_t band_size = 250;
-    // const char *hello_world = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+    const char *hello_world = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 
     uint8_t *band = calloc(band_size, sizeof(uint8_t));
     if (band == NULL)
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
         return EXIT_FAILURE;
     }
 
-    int bf_exit = bf_execute("[+]", band, band_size);
+    int bf_exit = bf_execute(hello_world, band, band_size);
 
     switch (bf_exit)
     {
